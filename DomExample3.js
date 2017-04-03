0532b098c5c62279932606a6037f3b302e683453
chriscaraway <chrisccaraway@gmail.com>
2017-04-03 16:30:07 -0400

    update

--- DomExample3.js
+++ DomExample3.js
@@ -1,2 +1,4 @@
-window.AddEventListener("keypress", function() {
+window.addEventListener("keypress", function(event) {
+		document.getElementById("Example3").innerHTML += String.fromCharCode(event.charCode);
+
 });
